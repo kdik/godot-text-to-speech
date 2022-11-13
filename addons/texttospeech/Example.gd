53d0731648dd2c71c91eb5bd74296cb66fe284e7
karolis dikcius <karolis.dikcius@gmail.com>
2022-11-13 14:52:40 +0200

    fixed the example script

--- addons/texttospeech/Example.gd
+++ addons/texttospeech/Example.gd
@@ -5,6 +5,6 @@ func _ready():
     var speed = 0.9
     yield($TextToSpeech.say(text, TextToSpeech.VOICE_AWB, speed), "completed")
     yield($TextToSpeech.say(text, TextToSpeech.VOICE_KAL, speed), "completed")
-    yield($TextToSpeech.say(text, TextToSpeech.VOICE_RMS, speed), "completed")
+    yield($TextToSpeech.say(text, TextToSpeech.VOICE_KAL16, speed), "completed")
     yield($TextToSpeech.say(text, TextToSpeech.VOICE_SLT, speed), "completed")
 
